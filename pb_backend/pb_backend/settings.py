42adfa152f0b5616f8505879973cfb29200a9ed9
Paul Bodner <bodner80@gmail.com>
2023-12-21 10:19:59 -0800

    altered allowed hosts

--- pb_backend/pb_backend/settings.py
+++ pb_backend/pb_backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', 'pb-v4-backend-a86d54467a96.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', 'pb-v4-backend-a86d54467a96.herokuapp.com']
 
 
 # Application definition
